d38a7037a7e81671670bcafa294ce028d3f56487
fuliping1 <email@example.com>
2018-01-19 20:26:58 +0800

    wokrerman

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user', function (Request $request) {
+    var_dump(getallheaders());
+//    return $request->user();
 });
